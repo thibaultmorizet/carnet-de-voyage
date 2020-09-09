834b4ee7e4c2eb5f652cd3b42ee00c76a8affaa5
Monsieur-Toorop <sebtoorop@gmail.com>
2020-09-09 19:57:03 +0200

    send activation mail with token

--- Back/api-carnet/src/Controller/MailerController.php
+++ Back/api-carnet/src/Controller/MailerController.php
@@ -6,21 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 class MailerController extends AbstractController
 {
     /**
      * @Route("/email")
      */
-    public function sendEmail(MailerInterface $mailer, $userMail, $subject, $text)
+    public function sendEmail(MailerInterface $mailer, $userMail, $subject, $html, $username, $token)
     {
-        $email = (new Email())
-            ->from('hello@example.com')
+        $email = (new TemplatedEmail())
+            ->from('carnetdevoyage@example.com')
             ->to($userMail)
             ->subject($subject)
-            ->text($text);
-
+            ->htmlTemplate($html)
+            ->context([
+                'username' => $username,
+                'token' => $token
+            ]);
+            
         $mailer->send($email);
     }
 }
--- Back/api-carnet/src/Controller/UserApiController.php
+++ Back/api-carnet/src/Controller/UserApiController.php
@@ -3,24 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-/**
- * @Route("/api/user")
- */
+
 class UserApiController extends AbstractController
 {
     /**
-     * @Route("/register", name="user_register", methods={"POST"})
+     * @Route("/api/user/register", name="user_register", methods={"POST"})
      */
-    public function register(SerializerInterface $serializer, Request $request, ValidatorInterface $validator, MailerInterface $mailerInterface, MailerController $mailerController)
+    public function register(UserPasswordEncoderInterface $passwordEncoder, SerializerInterface $serializer, Request $request, ValidatorInterface $validator, MailerInterface $mailerInterface, MailerController $mailerController)
     {
         try {
             $user = $serializer->deserialize(
@@ -49,12 +49,14 @@ class UserApiController extends AbstractController
                 Response::HTTP_BAD_REQUEST
             );
         }
-
+        $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($password);
+        $user->setToken(md5(uniqid()));
         $manager = $this->getDoctrine()->getManager();
         $manager->persist($user);
         $manager->flush();
 
-        $mailerController->sendEmail($mailerInterface, $user->getEmail(), 'Validation', 'Merci de valider votre adresse mail');
+        $mailerController->sendEmail($mailerInterface, $user->getEmail(), 'Merci de vous inscrire!', 'emails/activation.twig.html', $user->getFirstName(), $user->getToken());
 
         return $this->json(
             [
@@ -62,6 +64,36 @@ class UserApiController extends AbstractController
                 "id" => $user->getId()
             ],
             Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * @Route("/activation/{token}", name="activation")
+     */
+    public function activation($token, UserRepository $userRepository)
+    {
+        $user = $userRepository->findOneBy(['token' => $token]);
+
+        if (!$user) {
+            return $this->json(
+                [
+                    "success" => false,
+                    "errors" => 'Cet utilisateur n\'existe pas.'
+                ],
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $user->setToken(null);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        return $this->json(
+            [
+                "success" => true,
+                "id" => $user->getId(),
+            ],
         );
     }
 }
