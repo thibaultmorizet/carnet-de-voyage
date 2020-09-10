7338408fab79530b1001331944715f90b406f9e3
Quentin-Garcia <quentin.garciapro1999@gmail.com>
2020-09-10 16:25:40 +0200

    dezd

--- Front/src/middleware/register.js
+++ Front/src/middleware/register.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { REGISTER } from 'src/actions/register';
+import { REGISTER } from '../actions/register';
 
 const Register = (store) => (next) => (action) => {
   switch (action.type) {
