4c9d0e398533e6334513b14b1090a7e150b6af3c
chloeTalour <talour.chloe@gmail.com>
2020-09-10 22:08:44 +0200

    i continue the css

--- Front/src/components/CreateStep/map.js
+++ Front/src/components/CreateStep/map.js
@@ -13,24 +13,27 @@ const MapElement = () => {
     console.log('salut', evt.target);
   };
   return (
-    <Map className="map" center={[48.858884469956905, 2.346941100000031]} zoom={12} onClick={essai}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <GeoSearchControlElement
-        provider={prov}
-        showMarker
-        showPopup={false}
-        popupFormat={({ query, result }) => result.label}
-        maxMarkers={3}
-        retainZoomLevel={false}
-        animateZoom
-        autoClose={false}
-        searchLabel="Enter address, please"
-        keepResult
-      />
-    </Map>
+    <div>
+      <p className="littleMapTitle">Ajouter une localisation :</p>
+      <Map className="map" center={[48.858884469956905, 2.346941100000031]} zoom={12} onClick={essai}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoSearchControlElement
+          provider={prov}
+          showMarker
+          showPopup={false}
+          popupFormat={({ query, result }) => result.label}
+          maxMarkers={3}
+          retainZoomLevel={false}
+          animateZoom
+          autoClose={false}
+          searchLabel="Entrer une adresse"
+          keepResult
+        />
+      </Map>
+    </div>
 
   );
 };
