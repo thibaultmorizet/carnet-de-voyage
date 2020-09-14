d467d6791bc6ed440fadf9c3affb274cdd391f29
chloeTalour <talour.chloe@gmail.com>
2020-09-14 16:57:07 +0200

    token in progress

--- Front/src/middleware/login.js
+++ Front/src/middleware/login.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { LOGIN } from '../actions/login';
+import { keepToken } from '../actions/keepToken';
 
 const Login = (store) => (next) => (action) => {
   switch (action.type) {
@@ -9,12 +10,12 @@ const Login = (store) => (next) => (action) => {
       axios.post('http://34.239.44.174/api/login_check', {
         last_name: state.register.last_name,
         first_name: state.register.first_name,
-        email: state.register.email,
+        username: state.register.email,
         password: state.register.password,
-      },
-      { withCredentials: true })
+      })
         .then((response) => {
-          console.log(response);
+          const tokenString = JSON.parse(response.request.response);
+          store.dispatch(keepToken(tokenString.token));
         })
         .catch((error) => console.log(error));
       break;
