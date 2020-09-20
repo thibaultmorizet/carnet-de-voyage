b1418eface1a91a61af5f496d0d172336e021c8e
chloeTalour <talour.chloe@gmail.com>
2020-09-20 18:56:52 +0200

    little fix with button return

--- Front/src/components/UpdateStep/formUpdateStep.js
+++ Front/src/components/UpdateStep/formUpdateStep.js
@@ -3,8 +3,10 @@
 import React, { useEffect } from 'react';
 import Map from 'src/components/Map';
 import FormInput from 'src/components/FormInput';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useHistory, Link } from 'react-router-dom';
 import { useToasts } from 'react-toast-notifications';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import Spinner from 'src/components/Spinner';
 import { handlePicture, toastNotification } from 'src/selectors/carnetDeVoyage';
 import Modal from 'react-modal';
@@ -84,12 +86,16 @@ const FormUpdateStep = ({
   };
 
   return (
-
-    <div className="FormUpdateStep">
-      {loading && (
-      <Spinner />
-      )}
-      {!loading
+    <>
+      <Link to={`/travel/${id}`} className="travelPage__header--return updatePageReturn">
+        <FontAwesomeIcon icon={faArrowCircleLeft} />
+        <p>Revenir au voyage</p>
+      </Link>
+      <div className="FormUpdateStep">
+        {loading && (
+        <Spinner />
+        )}
+        {!loading
         && (
         <form action="" className="FormUpdateStep__formElt" onSubmit={handleOnSubmit}>
 
@@ -160,7 +166,8 @@ const FormUpdateStep = ({
 
         </form>
         )}
-    </div>
+      </div>
+    </>
   );
 };
 
