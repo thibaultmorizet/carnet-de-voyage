3e077ffc81cd500338b9da2cf0137cf7f0fefee9
thibaultmorizet <thibaultmorizet@yahoo.fr>
2020-09-21 13:22:08 +0200

    add Use for assert

--- Back/api-carnet/src/Entity/Picture.php
+++ Back/api-carnet/src/Entity/Picture.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\PictureRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
--- Back/api-carnet/src/Entity/Step.php
+++ Back/api-carnet/src/Entity/Step.php
@@ -7,6 +7,7 @@ use App\Repository\StepRepository;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 
