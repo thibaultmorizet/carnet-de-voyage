b90c29d4a52666d757aec36f9843f92e2571dd36
thibaultmorizet <thibaultmorizet@yahoo.fr>
2020-09-21 11:00:46 +0200

    Add service checkAuthorization

--- Back/api-carnet/src/Service/CheckAuthorization.php
+++ Back/api-carnet/src/Service/CheckAuthorization.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Travel;
 use App\Repository\StepRepository;
 use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
@@ -10,49 +11,76 @@ class CheckAuthorization
 {
 
     /**
-     * Get the Token from the headers and check the Username in the database
-     *
      * @param JWTEncoderInterface $jWTEncoderInterface
      * @param UserRepository $userRepository
-     * @return userArray [userId, admin(false or true), follow(array of id of follow travels)] or null (Bab Token or username not found)
+     * @param Travel $travel
+     * @return userArray [user, creator(false or true), admin(false or true), connected(false or true), follow(false or true)] or null (Bab Token or username not found)
      */
-    public static function checkAuthorization(JWTEncoderInterface $jWTEncoderInterface, UserRepository $userRepository)
+    public static function checkAuthorization(JWTEncoderInterface $jWTEncoderInterface, UserRepository $userRepository, Travel $travel)
     {
         // get the Token from the headers
         $token = apache_request_headers()["Authorization"];
+        //if there is no token
         if ($token == null) {
+            //we return "null"
             return null;
-        } // not Token
-        // delete sub string "Berear "
+        } 
+        //we delete sub string "Berear "
         $token = substr($token, 7);
-        // Decode Token
+        //we Decode the token
         $tokenArray = $jWTEncoderInterface->decode($token);
-        // gets a User object from username
+        //we recover the user object by the username
         $user = $userRepository->findOneByUsername($tokenArray['username']);
+        //if there is no user
         if ($user == null) {
+            //we return "null"
             return null;
-        } // username not found in Database
-        $userArray["userId"] = $user->getId();
-        $userArray["admin"] = false;
-        //dd($userArray);
-        // loop on all roles
-        $arrayRoles = $user->getRoles();
-        foreach ($arrayRoles as $roles) {
-            if ($roles == "ROLE_ADMIN") $userArray["admin"] = true;
+        } 
+        //we put the $user object in $userArray["user"]
+        $userArray["user"] = $user;
+        //we put the value false in $userArray["creator"]
+        $userArray["creator"] = false;
+        //if the id of the user is same of the id of the creator of the travel
+        if ($user->getId() == $travel->getCreator()->getId()) {
+            //we put the value true in $userArray["creator"]
+            $userArray["creator"] = true;
         }
+        //we put the value false in $userArray["admin"]
+        $userArray["admin"] = false;
+        //we put the value false in $userArray["connected"]
+        $userArray["connected"] = false;
+        //we recover the roles of the connected user 
+        $arrayRoles = $user->getRoles();
+        //for each role of $arrayRoles
+        foreach ($arrayRoles as $roles) {
+            //if the connected user is an administrator, we put the value true in $userArray["admin"]
+            if ($roles == "ROLE_ADMIN") $userArray["admin"] = true;
+            //if the user is connected, we put the value true in $userArray["connected"]
+            if ($roles == "ROLE_USER") $userArray["connected"] = true;
+        }
+        //we recover the travel objects that the user follows 
         $followTravels = $user->getFollower()->getValues();
         $followTravelsId = array();
+        //for each travel of $followTravels
         foreach ($followTravels as $travel) {
+            //we add the id of the travel in $followTravelsId
             array_push($followTravelsId, $travel->getId());
         }
-        $userArray["follow"] = $followTravelsId;
-        //dd($user, $userArray);       
+        //we put the value false in $userArray["follow"]
+        $userArray["follow"] = false;
+        //for each id of $followTravelsId
+        foreach ($followTravelsId as $travelId) {
+            //if the id is same of the id of the travel
+            if ($travel->getId() == $travelId) {
+                //we put the value true in $userArray["follow"]
+                $userArray["follow"] = true;
+            }
+        }
+        //we return the data of $userArray
         return $userArray;
     }
 
     /**
-     * Get the Token from the headers and check the Username in the database
-     *
      * @param travelId $travelId
      * @param stepId $stepId
      * @param StepRepository $stepRepository
@@ -60,10 +88,14 @@ class CheckAuthorization
      */
     public static function checkTravelStep($travelId, $stepId, StepRepository $stepRepository)
     {
-        $match=false;
-        if($stepRepository->find($stepId)->getTravel()->getId()==$travelId){
-            $match=true;
+        //we put the value false in $match
+        $match = false;
+        //if this step is a part of this travel
+        if ($stepRepository->find($stepId)->getTravel()->getId() == $travelId) {
+            //we put the value true in $match
+            $match = true;
         }
+        //we return $match
         return $match;
     }
 }
