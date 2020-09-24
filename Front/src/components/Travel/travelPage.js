6047fad132657e81dc3c2c77993a4710fcba25c8
Monsieur-Toorop <sebtoorop@gmail.com>
2020-09-25 00:35:14 +0200

    copy url OK, css WIP

--- Front/src/components/Travel/travelPage.js
+++ Front/src/components/Travel/travelPage.js
@@ -44,30 +44,6 @@ const TravelPage = ({
     fetchDataForUrlShare(id);
   };
 
-  function copyText (element) {
-    var range, selection, worked;
-
-    if (document.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText(element);
-      range.select();
-    } else if (window.getSelection) {
-      selection = window.getSelection();
-      range = document.createRange();
-      range.selectNodeContents(element);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    }
-
-    try {
-      document.execCommand('copy');
-      alert('text copied');
-    }
-    catch (err) {
-      alert('unable to copy text');
-    }
-  }
-
   return (
     <div className="travelPage">
       {loading && (
@@ -103,7 +79,7 @@ const TravelPage = ({
             {Cookies.get('loggedIn') && type === undefined && (
             <div className="travelPage__shareDiv">
               <input type="button" className="travelPage__header--addStep shareTravel" value="Créer un lien de partage" onClick={shareTravel} />
-              <input type="button" className="travelPage__header--addStep shareUrlCopy" value="Copie moi" onClick={copyText} />
+              <input type="button" id="btnCopy" className="travelPage__header--addStep shareUrlCopy" value="Copie moi" data-clipboard-target="#copyMe" />
             </div>
             )}
 
